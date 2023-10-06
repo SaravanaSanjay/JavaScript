cf23ad68b63505ea76a3aa92f43ce30e781bc016
SaravanaSanjay <145118627+SaravanaSanjay@users.noreply.github.com>
2023-10-06 15:46:30 +0530

    Add files via upload
    Add Bar chart

---
+++ chart.js
@@ -0,0 +1,36 @@
+var data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+    datasets: [{
+        label: 'Number of Items',
+        data: [1, 1, 13, 5, 22],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 2
+    }]
+};
+
+// Create a bar chart
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
